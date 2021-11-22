61a8c366c7266e7cda1be7b6b01c36d3be55f578
unknown <bramgitaris@gmail.com>
2021-11-22 15:47:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pratikum6/MyWorker.java
@@ -0,0 +1,47 @@
+package com.example.pratikum6;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import com.example.pratikum6.R;
+
+public class MyWorker extends Worker {
+
+    public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        displayNotification("Tangi o", "tangi");
+        return Result.success();
+    }
+
+    private void displayNotification(String task, String desc) {
+
+        NotificationManager manager =
+                (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationChannel channel = new NotificationChannel("test123",
+                "test123", NotificationManager.IMPORTANCE_HIGH);
+        manager.createNotificationChannel(channel);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(),
+                "test123")
+                .setContentTitle(task)
+                .setContentText(desc)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE);
+
+
+        manager.notify(1, builder.build());
+    }
+}
